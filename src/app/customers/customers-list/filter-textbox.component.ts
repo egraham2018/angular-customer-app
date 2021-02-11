3e2d559c147bdcab282e9627b34c37f0512ee307
egraham <78051646+liz-graham@users.noreply.github.com>
2021-02-11 13:54:04 -0500

    adding filter

--- src/app/customers/customers-list/filter-textbox.component.ts
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'filter-textbox',
   template: `
-    Filter: <input type="text" />
+    Filter: <input type="text" [(ngModel)]="filter" />
   `
 })
 export class FilterTextboxComponent implements OnInit {
+
+  // tslint:disable-next-line:variable-name
+  private _filter = '';
+  @Input() get filter(): string {
+    return this._filter;
+  }
+
+  set filter(val: string) {
+    this._filter = val;
+    this.changed.emit(this.filter);
+  }
+
+  @Output() changed: EventEmitter<string> = new EventEmitter<string>();
+
 
   constructor() {}
 
