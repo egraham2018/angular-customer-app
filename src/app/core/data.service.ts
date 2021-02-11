7ebabbb921b3759cf850dd4aa2121acdc4de68e1
egraham <78051646+liz-graham@users.noreply.github.com>
2021-02-11 14:48:07 -0500

    adding core service module
    provides examples of observable, error piping, etc.

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,18 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-// @ts-ignore
-import { Observable } from 'rxjs/Observable';
 import { map, catchError } from 'rxjs/operators';
 
 import { ICustomer, IOrder } from '../shared/interfaces';
+import { Observable } from 'rxjs';
 
-// @Injectable()
+@Injectable()
 export class DataService {
+
+  constructor(private http: HttpClient) { }
 
   baseUrl = 'assets/';
 
-  constructor() { }
+  getCustomers(): Observable<ICustomer[]> {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  // tslint:disable-next-line:typedef
+  getCustomer(id: number) {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(
+        map(customers => {
+          const customer = customers.filter((cust: ICustomer) => cust.id === id);
+          return (customer && customer.length) ? customer[0] : null;
+        }),
+        catchError(this.handleError)
+      );
+  }
+
+  getOrders(id: number): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl + 'orders.json')
+      .pipe(
+        map(orders => {
+          const custOrders = orders.filter((order: IOrder) => order.customerId === id);
+          return custOrders;
+        }),
+        catchError(this.handleError)
+      );
+  }
 
   // tslint:disable-next-line:typedef
   private handleError(error: any) {
@@ -25,5 +54,6 @@ export class DataService {
     }
     return Observable.throw(error || 'Node.js server error');
   }
+
 
 }
