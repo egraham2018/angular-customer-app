60529e87425737c54afed15a6100e035a900612c
egraham <78051646+liz-graham@users.noreply.github.com>
2021-02-12 18:26:10 -0500

    Routing!
    now localhost:4200 redirects to localhost:4200/customers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: ' <app-customers></app-customers>',
+  template: ' <router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
